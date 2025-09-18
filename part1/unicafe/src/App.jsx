24e05c15a0cb8c3c0c78ff536ccbdb7cf54d0300
oferreirap <o.ferreirap@hotmail.com>
2025-09-17 20:53:48 -0500

    anecdotes first part

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,10 @@ const Button = (props) => {
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+    </tr>
   )
 }
 const Statistics = (props) => {
@@ -21,14 +24,16 @@ const Statistics = (props) => {
   }
   else {
     return (
-      <div>
+      <table>
+        <tbody>
         <StatisticLine text="good" value={props.good} />
         <StatisticLine text="neutral" value={props.neutral} />
         <StatisticLine text="bad" value={props.bad} />
         <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
         <StatisticLine text="average" value={(((props.good*1)+(props.neutral*0)-(props.bad*1))/(props.good+props.neutral+props.bad))} />
         <StatisticLine text="positive" value={((props.good*1)/(props.good+props.neutral+props.bad))*100} />
-      </div>
+        </tbody>
+      </table>
     )
   }
 }
